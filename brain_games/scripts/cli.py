1ed34d37176a88d992cd958f242ac80276ef0d51
Tatka1803 <tanyaak16@gmail.com>
2024-12-01 19:59:03 +0400

    .

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,5 +1,6 @@
-import prompt # type: ignore
+import prompt
 
-def welcome_user(name) :
-  name = prompt.string('May I have your name? ')
-  print(f"Hello, {name}!")
+
+def welcome_user(): 
+    name = prompt.string('May I have your name? ')
+    print('Hello, {name}!')
