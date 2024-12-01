8729f24ccdc24d8df4a0d7565c2c29f07f7cc19f
Tatka1803 <tanyaak16@gmail.com>
2024-12-01 19:48:09 +0400

    .

--- brain_games/scripts/brain_games1.py
+++ brain_games/scripts/brain_games1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from brain_games.scripts.cli import welcome_user
 
 def greet():
     print("Welcome to the Brain Games!")
@@ -11,5 +10,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-def welcome_user(name) :
-  print(f"Hello, {name}!")
+from brain_games.scripts.cli import welcome_user
