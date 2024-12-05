8b0d4284292af437cc2429d0461140f1935f0ee8
Tatka1803 <tanyaak16@gmail.com>
2024-12-05 18:19:37 +0400

    .

---
+++ brain_games/engine.py
@@ -0,0 +1,22 @@
+from brain_games.scripts.cli import welcome_user, get_answer
+
+MAX_SCORE = 3
+
+
+def run_game(game):
+    score = 0
+    name = welcome_user(game.RULES)
+    while score < MAX_SCORE:
+        question, correct = game.run_game()
+        answer = get_answer(question)
+        if answer != correct:
+            print(
+                f"'{answer}' is wrong answer ;(.",
+                f"Correct answer was '{correct}'.",
+            )
+            print(f"Let's try again, {name}!")
+            return
+        print("Correct")
+        score += 1
+
+    print(f"Congratulations, {name}!")
--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -3,4 +3,10 @@ import prompt
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
-    print('Привет, ' + name + '!')
+    print('Hello, ' + name + '!')
+
+
+def get_answer(question):
+    print(f"Question: {question}")
+    answer = prompt.string("Your answer: ")
+    return answer
