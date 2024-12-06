cdc402caaf3b0675e6f330dcc209d9dab60ded05
Tatka1803 <tanyaak16@gmail.com>
2024-12-06 14:51:02 +0400

    .

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,9 @@
-from brain_games.engine import game_run
+from brain_games.engine import run_game
 from brain_games.games import calc
 
 
 def main():
-    game_run(calc)
+    run_game(calc)
 
 
 if __name__ == "__main__":
